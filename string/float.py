8fde9dd58bc0826307ae12e07f1edefa0a5ff0dc
rinki <shrivastavarinki283@gmail.com>
2024-05-14 17:40:20 +0530

    adding float file

---
+++ string/float.py
@@ -0,0 +1,20 @@
+# Float variables
+num1 = 2
+num2 = 10
+
+# Basic Arithemetic
+result1 = num1 + num2
+print("Addition is", result1)
+
+result2 = num1 - num2
+print("Substraction is", result2)
+
+result3 = num1 * num2
+print("Multiplication is", result3)
+
+result4 = num1 / num2
+print("Division is", result4)
+
+# Rounding
+result5 = round(3.14159265359, 2)           # Rounds to 2 decimal places
+print("Rounded", result5)
