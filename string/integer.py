134d4875e4047dad6198227249bde213e67294b7
rinki <shrivastavarinki283@gmail.com>
2024-05-14 17:57:45 +0530

    adding integer file

---
+++ string/integer.py
@@ -0,0 +1,17 @@
+# Integer variables
+num1 = 2
+num2 = 4
+print("value of num1 is", num1)
+print("value of num2 is", num2)
+
+# Integer division
+result1 = num1 // num2
+print("Division is", result1)
+
+# Modulus (Reminder)
+result2 = num1 % num2
+print("Modulus (Reminder) is ", result2)
+
+# Absolute value
+result3 = abs(-7)
+print("Absolute value is ", result3)
